01bd757dedc46a3be29f05c5864098ed3c5b1363
Dheeraj <er.dm.zer03@gmail.com>
2026-01-18 21:31:32 +0530

    RouteAll

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { Login } from './login/login';
+import { Contactus } from './contactus/contactus';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:'',component:Home},
+    {path:'about',component:About},
+    {path:'login',component:Login},
+    {path:'contactus',component:Contactus},
+];
